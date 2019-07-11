b52e471971911140ca982e0b8ad7a62744b0a722
choumo <309609425@qq.com>
2019-07-11 18:59:25 +0800

    22

--- abb_app_00/src/components/Abb/Text.vue
+++ abb_app_00/src/components/Abb/Text.vue
@@ -2,7 +2,7 @@
     <div>
       这是测试用的  
       客户端的Vue公共组件咱们都放在abb_sever_00/src/components/Abb文件夹下
-      页面组件放在abb_sever_00/src/views文件夹下
+      页面组件放在abb_sever_00/src/views文件夹下111
     </div>
 </template>
 <script>
