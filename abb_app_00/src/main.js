eb67d0b4880d8e53f48b2341be6df1df68df0d17
sp <1181139298@qq.com>
2019-07-11 08:55:38 +0800

    测试Vue

--- abb_app_00/src/main.js
+++ abb_app_00/src/main.js
@@ -6,9 +6,11 @@ import store from './store'
 import axios from 'axios'
 //将axios对象强行添加到Vue对象的原型对象中
 Vue.prototype.axios=axios;
-
+//引入elementUI
+import ElementUI from 'element-ui'
+import '../node_modules/element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
   router,
   store,
--- abb_app_00/src/router.js
+++ abb_app_00/src/router.js
@@ -1,19 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import Text from './components/Abb/Text.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {path:'/text',component:Text},
     {
       path: '/',
-      name: 'home',
       component: Home
     },
     {
       path: '/about',
-      name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
