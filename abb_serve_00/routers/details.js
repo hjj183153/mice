81509dd844ffbed8db4e5486431ff55e8850ccf1
sjk <sjk@.com>
2019-07-11 20:28:36 +0800

    修改

--- abb_serve_00/routers/details.js
+++ abb_serve_00/routers/details.js
@@ -2,7 +2,7 @@ const express=require('express');
 const pool=require('../pool.js');
 var router=express.Router();
 //房屋详情接口
-server.get("/details",(req,res)=>{
-  
+router.get("/",(req,res)=>{
+  var uid=req.query.uid;
 })
 module.exports=router;
