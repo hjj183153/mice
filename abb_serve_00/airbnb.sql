dcc20a94b5a2be2b8e286ec1dcf0042c86603d7e
choumo <309609425@qq.com>
2019-07-11 19:13:03 +0800

    更新两个表及数据

--- abb_serve_00/airbnb.sql
+++ abb_serve_00/airbnb.sql
@@ -75,3 +75,22 @@ CREATE TABLE Airbnb_Carousel(
  Carousel_id INT PRIMARY KEY AUTO_INCREMENT,
  Carousel_imgurl VARCHAR(128)
 );
+#房源类型表 zm
+CREATE TABLE Airbnb_housingResources_Type(
+ housingResources_id INT PRIMARY KEY AUTO_INCREMENT,
+ housingResources_name VARCHAR(64)
+);
+#出租类型表 zm
+CREATE TABLE Airbnb_Rent_Type(
+ Rent_id INT PRIMARY KEY AUTO_INCREMENT,
+ Rent_name VARCHAR(64),
+ Rent_about VARCHAR(64)
+);
+#房源类型表数据 zm
+INSERT INTO Airbnb_housingResources_Type VALUES(NULL,'公寓型住宅'),
+(NULL,'独栋房'),(NULL,'别墅'),(NULL,'Loft'),(NULL,'酒店'),(NULL,'酒店式公寓'),(NULL,'精品酒店'),(NULL,'青年旅舍'),(NULL,'农家乐'),(NULL,'度假村'),(NULL,'乡村小屋'),(NULL,'蒙古包'),(NULL,'平房'),(NULL,'联排别墅'),(NULL,'木屋'),(NULL,'露营车/房车'),(NULL,'帐篷'),(NULL,'露营地'),(NULL,'树屋'),(NULL,'船屋'),(NULL,'茅屋'),(NULL,'冰屋'),(NULL,'城堡'),(NULL,'风车房'),(NULL,'灯塔'),(NULL,'巴士'),(NULL,'火车'),(NULL,'飞机');
+#出租类型表数据 zm
+INSERT INTO Airbnb_Rent_Type VALUES
+(NULL,'整个房源','房客独享整个房源，无需与您或他人共住'),
+(NULL,'独立房间','房客有自己的独立房间，与他人共享客厅等空间'),
+(NULL,'合住房间','房客住在与他人合用的卧室或公共区域');
