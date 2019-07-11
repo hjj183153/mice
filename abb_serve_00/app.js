0c327de945f0e6752008e0fa94f5756f2569765a
sjk <sjk@.com>
2019-07-11 19:54:47 +0800

    pool 路由器 分离

--- abb_serve_00/app.js
+++ abb_serve_00/app.js
@@ -3,36 +3,34 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 const session = require("express-session");
+const detailsRouter=require("./routers/details.js");
 //2:配置第三方模块
- //2.1:配置连接池
- var pool = mysql.createPool({
-   host:"127.0.0.1",
-   user:"root",
-   password:"",
-   port:3306,
-   database:"xz",
-   connectionLimit:15
- })
  //2.2:跨域
- var server = express();
- server.use(cors({
+var server = express();
+server.use(cors({
    origin:["http://127.0.0.1:8080",
    "http://localhost:8080"],
    credentials:true
- }))
+}))
  //2.3:session
- server.use(session({
-   secret:"128位字符串",
-   resave:true,
-   saveUninitialized:true
- }))
+server.use(session({
+  secret:"128位字符串",
+  resave:true,
+  saveUninitialized:true
+}))
  //指定静态目录
- server.use(express.static("public"))
- server.listen(3000);
+server.use(express.static("public"))
+ //创建body中间件
+server.use(bodyParser.urlencoded({
+	extended:false
+}));
+server.listen(3000);
  
  //测试一下服务器端
- server.get("/test",(req,res)=>{
-   var uname=req.query.uname;
-   res.send({code:200,msg:uname})
- })
+server.get("/test",(req,res)=>{
+  var uname=req.query.uname;
+  res.send({code:200,msg:uname})
+})
+
+server.use("/details",detailsRouter);
 
--- abb_serve_00/routers/details.js
+++ abb_serve_00/routers/details.js
@@ -1,4 +1,8 @@
+const express=require('express');
+const pool=require('../pool.js');
+var router=express.Router();
 //房屋详情接口
-server.use("/details",(req,res)=>{
+server.get("/details",(req,res)=>{
   
 })
+module.exports=router;
