1a83987e24f0a1a1b4a2f361642dc3e63138d8e7
sp <1181139298@qq.com>
2019-07-12 08:54:17 +0800

    s

--- abb_serve_00/routers/index.js
+++ abb_serve_00/routers/index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const pool=require('../pool.js');
 var router=express.Router();
 //轮播图
-server.get("/carousel",(req,res)=>{
+router.get("/carousel",(req,res)=>{
   
 })
 module.exports=router;
