c0f5687f74501eedee48c468ed28ea9bc443dae1
zmw <1661380598@qq.com>
2019-07-12 12:37:41 +0800

    000

--- abb_app_00/src/components/Abb/Text.vue
+++ abb_app_00/src/components/Abb/Text.vue
@@ -3,9 +3,8 @@
       这是测试用的  
       客户端的Vue公共组件咱们都放在abb_sever_00/src/components/Abb文件夹下
       页面组件放在abb_sever_00/src/views文件夹下111
-      zmmm
+      zmmmJIUYGG
       页面组件放在abb_sever_00/src/views文件夹下
-      我加一句
     </div>
 </template>
 <script>
