0cc97db6af4bc22d7a714d0f4df2f41ca6d473ae
sp <1181139298@qq.com>
2019-07-13 08:22:22 +0800

    添加图片和index.vue

--- abb_app_00/src/router.js
+++ abb_app_00/src/router.js
@@ -2,14 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Text from './components/Abb/Text.vue'
-import aaa from './views/aaa.vue'
+import Index from './views/Index.vue'
 import add_become_a_host_room from './views/add_become_a_host_room.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {path:'/index',component:Index},
     {path:'/text',component:Text},
-    {path:'/aaa',component:aaa},
     {path:'/add_become_a_host_room',component:add_become_a_host_room},
     {path: '/',component: Home},
     {
