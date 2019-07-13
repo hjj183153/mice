97b9a71daf226e8ae21848e9fc2856a149342199
sp <1181139298@qq.com>
2019-07-13 20:06:08 +0800

    axios报错

--- abb_app_00/src/views/Index.vue
+++ abb_app_00/src/views/Index.vue
@@ -1,8 +1,14 @@
 <template>
     <div>
         <!--引入头部组件-->
-        <my-header></my-header>
-        
+        <my-header class="position-abs"></my-header>
+        <div class="block">
+    <el-carousel height="529px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="small">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 
     </div>
 </template>
@@ -11,9 +17,37 @@ export default {
     data(){
         return {}
     },
+    created(){
+        this.getcarouserimg()
+    },
+    methods: {
+        getcarouserimg(){
+            this.axios.get("index/Carousel").then(result=>{
+                console.log(result)
+            })
+        }
+    },
     
 }
 </script>
 <style scoped>
-    
+.position-abs{
+    position:absolute;
+    z-index:10;
+}
+    el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 150px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+     background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+     background-color: #d3dce6;
+  }
 </style>
