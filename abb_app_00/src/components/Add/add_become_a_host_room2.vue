51030cd5490a62a449eff5da8e3006614da1ff3c
choumo <309609425@qq.com>
2019-07-14 15:47:10 +0800

    成为房东2更新

--- abb_app_00/src/components/Add/add_become_a_host_room2.vue
+++ abb_app_00/src/components/Add/add_become_a_host_room2.vue
@@ -13,15 +13,15 @@
                         <p>最多容纳房客人数</p>
                     </div>
                     <div class="btn_add">
-                        <div>
-                            <span class="div_btn_add">
+                        <div @click="people_count(-1)">
+                            <span class="div_btn_add" id="people_reduce">
                                 <svg viewBox="0 0 24 24" role="img" aria-label="subtract" focusable="false" style="height: 1em; width: 1em; display: block; fill: currentcolor;"><rect height="2" rx="1" width="12" x="6" y="11"></rect></svg>
                             </span>
                         </div>
                         <div id="people_count">
-                            <span>4</span>
+                            <span v-text="peoplecount">4</span>
                         </div>
-                        <div>
+                        <div  @click="people_count(1)">
                             <span class="div_btn_add">
                                 <svg viewBox="0 0 24 24" role="img" aria-label="add" focusable="false" style="height: 1em; width: 1em; display: block; fill: currentcolor;"><rect height="2" rx="1" width="12" x="6" y="11"></rect><rect height="12" rx="1" width="2" x="11" y="6"></rect></svg>
                             </span>
@@ -40,18 +40,20 @@
                 <div class="div_body5">
                     <div>
                         <p class="font-title5">有几张床？</p>
-                        <p class="font-title4">床铺数量</p>
-                    </div>                    
-                    <div class="btn_add">
-                        <div>
+                    </div> 
+                    <div class="font-title4">                  
+                        <p>床铺数量</p>
+                    </div> 
+                    <div class="btn_add" style="float:left" id="div_bed_count">
+                        <div @click="bed_count(-1)" style="opacity:0.3;">
                             <span class="div_btn_add">
                                 <svg viewBox="0 0 24 24" role="img" aria-label="subtract" focusable="false" style="height: 1em; width: 1em; display: block; fill: currentcolor;"><rect height="2" rx="1" width="12" x="6" y="11"></rect></svg>
                             </span>
                         </div>
-                        <div id="people_count">
-                            <span>1</span>
+                        <div id="bed_count">
+                            <span v-text="bedcount"></span>
                         </div>
-                        <div>
+                        <div  @click="bed_count(1)">
                             <span class="div_btn_add">
                                 <svg viewBox="0 0 24 24" role="img" aria-label="add" focusable="false" style="height: 1em; width: 1em; display: block; fill: currentcolor;"><rect height="2" rx="1" width="12" x="6" y="11"></rect><rect height="12" rx="1" width="2" x="11" y="6"></rect></svg>
                             </span>
@@ -59,8 +61,29 @@
                     </div>
                 </div>
                 <div class="div_body6">
-                    <btn_bd_g :text="按钮"></btn_bd_g>
+                    <h3>床型信息</h3>
+                    <p class="font-title3" style="margin:10px 0 20px">
+                        告知房客每个房间的床铺类型，有助于他们了解床型信息。
+                    </p>
                 </div>
+                <div class="div_body7">
+                    <div>
+                        <p>合用空间</p>
+                        <p class="font-title5" style="padding-top:2px;">0张床</p>
+                    </div>
+                    <div>
+                        <div id="addbed" @click="btn_addbed" v-show="addbed=='true'">
+                            添加床铺
+                        </div>
+                        <div id="addbed" v-show="addbed=='false'" style="font-weight:600">
+                            完成
+                        </div>
+                    </div>
+                </div>
+                <div class="div_body8">
+                    <div></div>
+                </div>
+                <div style="height:20px;"></div>
             </div>
             
         </div>
@@ -72,7 +95,9 @@ export default {
         return{
             Airbnb_House:{},
             bedrooms:[],
-            
+            bedcount:1,
+            peoplecount:4,
+            addbed:'false',
         }
     },
     props:{
@@ -101,8 +126,40 @@ export default {
             var select_bedroom=document.getElementById("select_bedroom")
             var div_select_bedroom=select_bedroom.parentNode;
             div_select_bedroom.style.border="0px solid transparent"
-        }
+        },
+        bed_count(n){
+            var div_bed_count=document.getElementById("div_bed_count")
+            var bed_reduce=div_bed_count.firstChild;
+            let count=this.bedcount
+            if(n==1){
+                bed_reduce.style.opacity="1"
+                count+=n;
+            }
+            if(n==-1&&count>2){
+                count+=n;
+            }else if(n==-1&&count==2){
+                bed_reduce.style.opacity="0.3"
+                count+=n;
+            }
+                this.bedcount=count;            
+        },
+        people_count(m){
+            var people_reduce=document.getElementById("people_reduce")
+            if(m==1){
+                people_reduce.style.opacity="1"
+                this.peoplecount+=m;
+                console.log(this.peoplecount)
+            }
+            if(m==-1&&this.peoplecount>2){
+                this.peoplecount+=m;
+            }else if(m==-1&&this.peoplecount==2){
+                people_reduce.style.opacity="0.3"
+                this.peoplecount+=m;
+            }                  
+        },
+        btn_addbed(){
 
+        }
     },
     
     
@@ -169,13 +226,14 @@ export default {
     cursor: pointer;
     border-radius: 50%;
     border:1px solid rgb(0,132,137);
-    /* text-align: center;
-    line-height: 32px; */
     position: relative;
 }
+.btn_add:after{
+    content:"";
+    display: block;
+    clear: both;
+}
 .div_btn_add>svg{
-     /* text-align: center;
-    line-height: 32px; */
     position: absolute;
     top:25%;
     left: 25%;
@@ -215,6 +273,62 @@ export default {
     color:#484848;
     border:1px solid #eee;
 }
-
-.div_body6{height:100px;}
+#bed_count{
+    width: 15%;
+    text-align: center;
+    vertical-align: middle;
+    height:34px;
+}
+#bed_count>span{
+    display: block;
+    margin:10px auto;
+    font-weight: 600;
+}
+.div_body6{
+    box-sizing: border-box;
+    padding: 30px 0;
+    text-align: left;
+    clear: left;
+}
+.div_body6>h3{
+    font-size:24px;
+    font-weight: normal;
+    color: #484848;
+}
+.div_body7{
+    border-top:1px solid #dce0e0;
+    border-bottom:1px solid #dce0e0;
+    padding-top: 28px;
+    padding-bottom : 28px;
+    color: #484848;
+}
+.div_body7:after{
+    content:"";
+    display: block;
+    clear: both;
+}
+.div_body7 p:first-child{
+    font-size: 19px;
+}
+.div_body7 div:first-child{
+    width:50%;
+    float: left;
+}
+.div_body7>div:last-child{
+    float: right;
+    width:30%;
+}
+#addbed{
+    float: right;
+    font-size: 18px;
+    font-weight: bold;
+    background: #fff;
+    border: 1px solid #c4c4c4;
+    cursor:pointer;
+    padding: 9px 27px;
+    border-radius: 4px;
+    line-height: 1.43;
+    text-align: center;
+    font-family: inherit;
+}
 </style>
