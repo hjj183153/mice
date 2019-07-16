5bc3e87bb5003c7d2fa26c0dea170909ea6cc610
choumo <309609425@qq.com>
2019-07-16 12:49:20 +0800

    aaa

--- abb_app_00/src/components/Add/add_become_a_host_room7.vue
+++ abb_app_00/src/components/Add/add_become_a_host_room7.vue
@@ -10,7 +10,13 @@
                     <p>为房源起一个能突出其独特之处的标题，吸引更多房客。</p>
                 </div>
                 <div class="div_body1">
-                    <input type="text" placeholder="房源名称">
+                    <input type="text" maxlength="50" placeholder="房源名称" v-model="House_name" @change="House_namechange" @keyup="housenamecount">
+                </div>
+                <div class="font-title3" style="margin-top:5px;">
+                    <p v-text="`剩余${namecount}个字符`"></p>
+                </div>
+                <div class="font-title3" style="margin-top:40px;">
+                    <p>好的房源标题可以增加房客的点击率，房屋名称可包含地标、位置、附近交通、房屋特色等信息。</p>
                 </div>
             </div>
 
@@ -21,11 +27,30 @@
 <script>
 export default {
      data(){return{
-        
+        House_name:"",
+         namecount:50,
     }},
     props:{
-        Airbnb_House:{default:""}
-    }
+        Airbnb_House:{House_name:""}
+    },
+    methods:{
+        House_namechange(){
+            this.Airbnb_House.House_name=this.House_name;
+            console.log(this.Airbnb_House.House_name)
+        },
+        housenamecount(){
+
+            this.namecount=50-this.House_name.length
+             console.log(this.namecount)
+
+        }
+    },
+    // computed:{
+    //     namecount(){            
+    //         this.namecount=50-this.House_name.length
+	// 				return this.namecount
+    //             }
+    // },
 }
 </script>
 <style scoped>
@@ -62,6 +87,12 @@ export default {
     margin-bottom: 32px;
 }
 .div_body1>input{
-    
+    padding: 20px;
+    width: 100%;
+    border:1px solid #dce0e0;
+    border-radius:3px;
+    box-sizing: border-box;
+    color:#484848;
+    font-size:100%;
 }
 </style>
