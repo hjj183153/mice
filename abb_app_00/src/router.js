90e4bc65307a0eefa727e71a85cf605f0ece48d4
lr <leeleelirui@gmail.com>
2019-07-17 18:47:28 +0800

    1

--- abb_app_00/src/router.js
+++ abb_app_00/src/router.js
@@ -5,13 +5,6 @@ import Text from './components/Abb/Text.vue'
 import Details from './views/Details.vue'
 import Index from './views/Index.vue'
 import add_become_a_host_room from './views/add_become_a_host_room.vue'
-<<<<<<< HEAD
-import Search from './views/search/Search.vue'
-import add_become_a_host_room2 from './components/Add/add_become_a_host_room2.vue'
-import Story from './components/Abb/Story.vue'
-import Search_result from './components/Abb/Search_result.vue'
-import userdata from './views/userdata.vue'
-=======
 import add_room from './components/Add/add_become_a_host_room1.vue'
 import add_bedrooms from './components/Add/add_become_a_host_room2.vue'
 import add_bathrooms from './components/Add/add_become_a_host_room3.vue'
@@ -23,7 +16,6 @@ import Story from './components/Abb/Story.vue'
 import Search_result from './components/Abb/Search_result.vue'
 import userdata from './views/userdata.vue'
 import userguide from './views/userguide.vue'
->>>>>>> 7e9c5ef5cf946babb242bddcf8b6a5d0bb3c9b7d
 
 Vue.use(Router)
 import login from './views/login.vue'
